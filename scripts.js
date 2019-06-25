4d0d26b54ca8e6917884c6c31fa1c5e94cae5d37
Enrique Jose Estrada Nunez <kikeestrada@gmail.com>
2019-06-24 21:43:17 -0600

    first load

---
+++ scripts.js
@@ -0,0 +1,42 @@
+const changeThemeButtons = document.querySelectorAll('[name=theme]')
+const rootStyles = document.documentElement.style
+
+const lightTheme = {
+    '--bg-color': "#eee",
+    '--bg-color-2': "#fff",
+    '--text-color': "#53575A",
+    '--accent-color': "#007bdf",
+    '--font': "'Open Sans', sans-serif"
+}
+
+const darkTheme = {
+    '--bg-color': "#53575A",
+    '--bg-color-2': "#282d31",
+    '--text-color': "#eee",
+    '--accent-color': "#FF3C32",
+    '--font': "'Lato', sans-serif"
+}
+
+
+for (let button of changeThemeButtons) {
+    button.addEventListener('change', e => {
+        if (e.target.id === 'dark') {
+            changeTheme(darkTheme)
+        } else if (e.target.id === 'light') {
+            changeTheme(lightTheme)
+        }
+    })
+}
+
+
+const changeTheme = theme => {
+    let cssVars = Object.keys(theme)
+    
+    for(let cssVar of cssVars) {
+        rootStyles.setProperty(cssVar, theme[cssVar])
+        
+    }
+}
+
+//  EJEMPLO
+console.log(darkTheme["--text-color"]);
